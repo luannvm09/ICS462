57398aad4ae4e13ad8daff32c8dd0bfe82625f35
Andrew <atvick21@gmail.com>
2021-11-14 15:47:43 -0600

    just made some functions but didn't do much

--- project.c
+++ project.c
@@ -1,7 +1,54 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define NORTH 0;
+#define SOUTH 1;
+#define car 3;
+#define van 4;
+
+sem_t bridge_lock;
+sem_t mutex;
+int weight;
+int car_southbound;
+int car_northbound;
+
+void vehicle-Routine(int vehicle-id, int vehicle-type, int direction)
+{
+	
+}
+
+void arrive()
+{
+
+}
+
+void cross()
+{
+
+}
+
+void leave()
+{
+
+]
+
+/*struct vehicle{
+	int vehicle_id;
+	int vehicle_type;
+	int direction;
+	int state;
+};
+
+struct bridge{
+	int weight;
+	struct vehicle northVehicles[30];
+	struct vehicle southVehicles[30];
+	bool twoDirections;
+};*/
 
 int main(){
-	printf("Testing push");
+		
 
 	return 0;
 }
