654fb32c85dd9e13cabe7e87013d7e608cc5dcad
Luan Nguyen <70990811+luannvm09@users.noreply.github.com>
2021-11-18 16:19:53 -0600

    Add the corrected codes
    New codes

--- luan.c
+++ luan.c
@@ -1,164 +1,233 @@
-#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <signal.h>
 #include <stdlib.h>
-#include <pthread.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/sem.h>
+#include "sem_ops.h"
 
-#define MAX_BR_CAP 5                 // max 5 vehicles allowed on the bridge
-#define CROSS_TIME 4                   // it takes 4sec to cross the bridge
-#define DIREC_PROB 0.7 //assumed probability of direction 0 for fair traffic control policy
+/*number of cars and trucks heading in each direction*/
+#define CARS_EAST 3
+#define CARS_WEST 3
+#define TRUCKS_EAST 2
+#define TRUCKS_WEST 2
 
-#define handle_err(s) do{perror(s); exit(EXIT_FAILURE);}while(0)
+/*max cars allowed on bridge at any one time*/
+#define MAXCARS 3
 
-/* structure representing threads */
+/*car or truck randomly arrives within first MAXWAIT seconds*/
+#define MAXWAIT 6
+/*and takes CROSSINGTIME to make is across the bridge*/
+#define CROSSINGTIME 4
 
-typedef struct _thread_argv
-{
-   int vid;
-   int direc;
-   int time_to_cross;
-} thread_argv;
+/* colors to be fancy like the example*/
+#define KORG  "\x1B[35m"
+#define RESET "\033[0m"
 
-/* structure representing bridge */
-typedef struct _bridge {
-   int dept_idx;
-   int num_car;
-   int curr_dir;
-} bridge_t;
+/*binary semaphores*/
+int count_lock_east;	/*protects cars_count_east*/
+int count_lock_west;	/*protects cars_count_west*/
+int bridge_lock;		/*protects the bridge*/
 
-void bridge_init();
-void bridge_destroy();
-void dispatch(int n);
-void *OneVehicle(void *argv);
-void ArriveBridge(int vid, int direc);
-void CrossBridge(int vid, int direc, int time_to_cross);
-void ExitBridge(int vid, int direc);
+int car_sem_east;		/*makes sure no more than MAXCARS cars on bridge heading east*/
+int car_sem_west;		/*makes sure no more than MAXCARS cars on bridge heading west*/
 
-pthread_t *threads = NULL;   /* Array to hold thread structs */
-thread_argv *args = NULL;   /* Array to hold thread arguments */
-int num_v = 30;           /* Total number of vehicles to be created */
+int cars_count_east;	/*num cars currently on bridge heading east*/
+int cars_count_west;	/*num cars currently on bridge heading west*/
 
-bridge_t br;           /* Bridge struct shared by the vehicle threads*/
-
-int main(int argc, char *argv[])
-{
-   int sched_opt;                     // number of vehicles threads to be created
-   int i;
-
-   if(argc < 2)
-   {
-       printf("Usage: %s SCHED_OPT [SEED]\n", argv[0]);
-       exit(EXIT_SUCCESS);
-   }
-
-   /* Process Arguments */
-   sched_opt = atoi(argv[1]);
-   if(argc == 3)
-       srand((unsigned int)atoi(argv[2]));
-   else
-       srand((unsigned int)time(NULL));
-
-   /* Allocate memory for thread structs and arguments */
-   if((threads = (pthread_t *)malloc(sizeof(pthread_t) * num_v)) == NULL)
-       handle_err("malloc() Failed for threads");
-   if((args = (thread_argv *)malloc(sizeof(thread_argv) * num_v)) == NULL)
-       handle_err("malloc() Failed for args");
-
-   /* Init bridge struct */
-   bridge_init();
-
-   /* Create vehicle threads */
-   switch(sched_opt)
-   {
-       case 1 : dispatch(5); break;
-       case 2 : dispatch(10); break;
-       case 3 : dispatch(30); break;
-       default:
-           fprintf(stderr, "Bad Schedule Option %d\n", sched_opt);
-           exit(EXIT_FAILURE);
-   }
-  
-   /* Join all the threads */
-   for(i = 0; i < num_v; i++)
-       pthread_join(threads[i], NULL);
-
-   /* Clean up and exit */
-   bridge_destroy();
-
-   exit(EXIT_SUCCESS);
+/*cleanup deletes semaphores at end*/
+void cleanup(void){
+	rm_sem(count_lock_east);
+	rm_sem(count_lock_west);
+	rm_sem(bridge_lock);
+	rm_sem(car_sem_east);
+	rm_sem(car_sem_west);
+	exit(1);
 }
 
-/**
-*   Create n vehicle threads for every 10 seconds until the total
-*    number of vehicles reaches num_v
-*   Each thread handle is stored in the shared array - threads
-*/
-void dispatch(int n)
-{
-int k, i;
-  
-   for(k = 0; k < num_v; k += n) //threads should be created till no of vehicles become num_v
-   {
-       printf("Dispatching %d vehicles\n", n);
+/*cars heading east*/
+void *car_east(void * arg){
+	int *me = (int *) arg;	/*id of particular car*/
 
-      // executed to create threads from k to k+n (0-n, n+1-n+n and so on till num_v)
+	sleep( rand() % MAXWAIT );	/*wait for a while before showing up at the bridge*/
+	
+	P(car_sem_east);	/*proceed when there's less then 3 cars
+						heading east on the bridge*/
+	P(count_lock_east);	/*lock counter access*/
+	cars_count_east++;	/*increment num cars heading east*/
+	if(cars_count_east == 1) 	/*I'm the first car heading east on the bridge*/
+		P(bridge_lock);			/*proceed when the bridge is free*/
+	V(count_lock_east);		/*unlock counter access*/
+	/*announce it*/
+	printf("Car "KORG"%d"RESET" going east "KORG"on"RESET" the bridge\n", *me );
+	/*proceed to cross the bridge*/
+	sleep( CROSSINGTIME );
+	/*announce it*/
+	printf("Car "KORG"%d"RESET" going east "KORG"off"RESET" the bridge\n", *me );
 
-       for( i = k; i < k + n && i < num_v; i++)
-       {
-           /* The probability of direction 0 is DIREC_PROB */
-           int direc = rand() % 1000 > DIREC_PROB * 1000 ? 0 : 1;
+	
+	P(count_lock_east);	/*lock counter access*/
+	cars_count_east--;	/*decrement counter*/
+	if(cars_count_east == 0)	/*if i'm the last car on the bridge*/
+		V(bridge_lock);		/*unlock the bridge for other traffic*/
+	V(count_lock_east);	/*unlock counter access*/
+	V(car_sem_east);	/*exit the bridge*/
+	pthread_exit( NULL );	/*exit the thread*/
+}
+/*cars heading west*/
+void *car_west(void * arg){
+	int *me = (int *) arg;	/*id of particular car*/
 
-           args[i] = (thread_argv){i, direc, CROSS_TIME};     // array to store thread arg which will be passed in pthread_create
-           if(pthread_create(threads + i, NULL, &OneVehicle, args + i) != 0)
-               handle_err("pthread_create Failed");
-       }
-      
-       printf("Sleep 10 seconds\n"); sleep(10);              // creating new threads every 10 seconds
-   }
+	sleep( rand() % MAXWAIT );	/*wait for a while before showing up at the bridge*/
+	
+	P(car_sem_west);	/*proceed when there's less then 3 cars
+						heading west on the bridge*/
+	P(count_lock_west);	/*lock counter access*/
+	cars_count_west++;	/*increment num cars heading west*/
+	if(cars_count_west == 1) /*I'm the first car heading west on the bridge*/
+		P(bridge_lock);	/*proceed when the bridge is free and lock bridge*/
+	V(count_lock_west);	/*unlock counter access*/
+
+	/*announce it*/
+	printf( "Car "KORG"%d"RESET" going west "KORG"on"RESET" the bridge\n", *me );
+	/*proceed to cross the bridge*/
+	sleep( CROSSINGTIME );
+	/*announce it*/
+	printf( "Car "KORG"%d"RESET" going west "KORG"off"RESET" the bridge\n", *me );
+
+	P(count_lock_west);	/*lock counter access*/
+	cars_count_west--;	/*decrement counter*/
+	if(cars_count_west == 0)	/*if i'm the last car on the bridge*/
+		V(bridge_lock);	/*unlock the bridge for other traffic*/
+	V(count_lock_west); /*unlock counter access*/
+	V(car_sem_west); 	/*exit the bridge*/
+	pthread_exit( NULL );	/*exit the thread*/
+}
+/*trucks heading east*/
+void *truck_east(void * arg){
+	int *me = (int *) arg; /*id of particular truck*/
+
+	sleep( rand() % MAXWAIT );	/*wait for a while before showing up at the bridge*/
+
+	P(bridge_lock); /*proceed when bridge is free, lock the bridge*/
+	/*announce it*/
+	printf( "Truck "KORG"%d"RESET" going east "KORG"on"RESET" the bridge\n", *me );
+	/*proceed to cross the bridge*/
+	sleep( CROSSINGTIME );
+	/*announce it*/
+	printf( "Truck "KORG"%d"RESET" going east "KORG"off"RESET" the bridge\n", *me );
+	V(bridge_lock); /*unlock the bridge*/
+	pthread_exit( NULL );	/*exit the thread*/
+}
+/*trucks heading west*/
+void *truck_west(void * arg){
+	int *me = (int *) arg; /*id of particular truck*/
+
+	sleep( rand() % MAXWAIT ); /*wait for a while before showing up at the bridge*/
+
+	P(bridge_lock); /*proceed when bridge is free, lock the bridge*/
+	/*announce it*/
+	printf( "Truck "KORG"%d"RESET" going west "KORG"on"RESET" the bridge\n", *me );
+	/*proceed to cross the bridge*/
+	sleep( CROSSINGTIME );
+	/*announce it*/
+	printf( "Truck "KORG"%d"RESET" going west "KORG"off"RESET" the bridge\n", *me );
+	V(bridge_lock); /*unlock the bridge*/
+	pthread_exit( NULL ); /*exit the thread*/
 }
 
-/* Life-cycle of one vehicle on the bridge */
+int main(void){
+	int i;	/*index*/
+	pthread_t car_tid_east[CARS_EAST];	/*thread ids of cars heading east*/
+	pthread_t car_tid_west[CARS_WEST];	/*thread ids of cars heading west*/
+	pthread_t truck_tid_east[TRUCKS_EAST]; /*thread ids of trucks heading east*/
+	pthread_t truck_tid_west[TRUCKS_WEST]; /*thread ids of trucks heading west*/
+	int car_id_east[CARS_EAST]; /*car ids of cars heading east*/
+	int car_id_west[CARS_WEST]; /*car ids of cars heading west*/
+	int truck_id_east[TRUCKS_EAST]; /*truck ids of trucks heading east*/
+	int truck_id_west[TRUCKS_WEST]; /*truck ids of trucks heading west*/
 
-void *OneVehicle(void *argv)
-{
-   thread_argv *args = (thread_argv *)argv;
+	/*initialise no. cars to 0*/
+	cars_count_east = 0;
+	cars_count_west = 0;
 
-   ArriveBridge(args->vid, args->direc);
-   CrossBridge(args->vid, args->direc, args->time_to_cross);
-   ExitBridge(args->vid, args->direc);
+	/*create semaphores*/
+	bridge_lock = semtran(IPC_PRIVATE);	
+	count_lock_east = semtran(IPC_PRIVATE);
+	count_lock_west = semtran(IPC_PRIVATE);
+	car_sem_east = semtran(IPC_PRIVATE);
+	car_sem_west = semtran(IPC_PRIVATE);
 
-   pthread_exit(0);
-}
+	/*binary semaphores, initalise to 1*/
+	V(count_lock_east);
+	V(count_lock_west);
+	V(bridge_lock);
 
-void bridge_init()
-{
-   br.dept_idx = 0;
-   br.curr_dir = 0;
-   br.num_car = 0;
+	/*initialise car semaphores to size MAXCARS*/
+	for(i = 0; i < MAXCARS; i++)
+		V(car_sem_east);
+	for(i = 0; i < MAXCARS; i++)
+		V(car_sem_west);
 
-   return;
-}
+	/*set ids of vehicles*/
+	for(i = 0; i < CARS_WEST; i++)
+		car_id_west[i] = i;
+	for(i = 0; i < CARS_EAST; i++)
+		car_id_east[i] = i;
+	for(i = 0; i < TRUCKS_EAST; i++)
+		truck_id_east[i] = i;
+	for(i = 0; i < TRUCKS_WEST; i++)
+		truck_id_west[i] = i;
 
-void bridge_destroy()
-{
-   return;
-}
+	/* create threads -- remembering to check for errors!*/
+	for(i = 0; i < CARS_WEST; i++)
+		if(pthread_create( &car_tid_west[i], NULL, car_west, (void *) &car_id_west[i] )){
+			perror("Could not create thread");
+			exit(EXIT_FAILURE);
+		}
+	for(i = 0; i < CARS_EAST; i++)
+		if(pthread_create( &car_tid_east[i], NULL, car_east, (void *) &car_id_east[i] )){
+			perror("Could not create thread");
+			exit(EXIT_FAILURE);
+		}
+	for(i = 0; i < TRUCKS_EAST; i++)
+		if(pthread_create( &truck_tid_east[i], NULL, truck_east, (void *) &truck_id_east[i] )){
+			perror("Could not create thread");
+			exit(EXIT_FAILURE);
+		}
+	for(i = 0; i < TRUCKS_WEST; i++)
+		if(pthread_create( &truck_tid_west[i], NULL, truck_west, (void *) &truck_id_west[i] )){
+			perror("Could not create thread");
+			exit(EXIT_FAILURE);
+		}
 
-void ArriveBridge(int vid, int direc)
-{
-   return;
-}
+	/*join threads*/
+	for(i = 0; i < CARS_WEST; i++)
+		if(pthread_join( car_tid_west[i], NULL )){
+			perror("Thread join failed");
+			exit(EXIT_FAILURE);		
+		}
+	for(i = 0; i < CARS_EAST; i++)
+		if(pthread_join( car_tid_east[i], NULL )){
+			perror("Thread join failed");
+			exit(EXIT_FAILURE);		
+		}
+	for(i = 0; i < TRUCKS_EAST; i++)
+		if(pthread_join( truck_tid_east[i], NULL )){
+			perror("Thread join failed");
+			exit(EXIT_FAILURE);		
+		}
+	for(i = 0; i < TRUCKS_WEST; i++)
+		if(pthread_join( truck_tid_west[i], NULL )){
+			perror("Thread join failed");
+			exit(EXIT_FAILURE);		
+		}
 
-void CrossBridge(int vid, int direc, int time_to_cross)
-{
-   fprintf(stderr, "vid=%d dir=%d starts crossing. Bridge num_car=%d curr_dir=%d\n",
-       vid, direc, br.num_car, br.curr_dir);
-   sleep(time_to_cross);
-   return;
-}
-
-void ExitBridge(int vid, int direc)
-{
-   fprintf(stderr, "vid=%d dir=%d exit with departure idx=%d\n",
-       vid, direc, br.dept_idx);
-   return;
+	/*delete semaphores*/
+	cleanup();
+	return 0;
+	
 }
